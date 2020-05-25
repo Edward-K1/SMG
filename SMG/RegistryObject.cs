eef54141f9d2d1b7e46bc5175a838063796ead98
Edward-K1 <eddyk1collab@gmail.com>
2020-05-26 00:12:07 +0300

    add registryObject

--- SMG/RegistryObject.cs
+++ SMG/RegistryObject.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace SMG
 {
     class RegistryObject
     {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string LaunchCommand { get; set; }
     }
 }
